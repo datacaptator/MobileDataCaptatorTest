1dd230d756132b55392bd79cdefea890fa6c9e1c
bartvanhoey <bartvanhoey@hotmail.com>
2014-03-26 11:39:55 +0100

    commit

--- MobileDataCaptatorTest/src/be/mobiledatacaptator/test/InitialTest.java
+++ MobileDataCaptatorTest/src/be/mobiledatacaptator/test/InitialTest.java
@@ -1,5 +1,6 @@
 package be.mobiledatacaptator.test;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 import android.app.Instrumentation;
@@ -62,98 +63,157 @@ public class InitialTest extends ActivityInstrumentationTestCase2<SelectProjectA
 			UnitOfWork unitOfWork = UnitOfWork.getInstance();
 			Project project = unitOfWork.getActiveProject();
 
-			if (project.isLoadPhotoActivity() != true) {
-				Log.e("PROJECT - No Photos", project.getName());
-				again = true;
-				solo.goBack();
-			} else {
-				Log.e("PROJECT - photos", project.getName());
+			int randomKeuze = 0; // random.nextInt(3);
 
-				solo.clickOnButton(solo.getString(be.mobiledatacaptator.R.string.button_open_photo));
-				solo.assertCurrentActivity("TakePhotoActivity", TakePhotoActivity.class);
+			int randomYesNo = random.nextInt(1);
 
-				ListView listViewPhotos = (ListView) solo.getView(be.mobiledatacaptator.R.id.listViewPhotos);
-				int countPhotos = listViewPhotos.getCount();
+			switch (randomKeuze) {
+			case 0: // open fiche
+				Log.e("OpenFiche", "OpenFiche");
 
-				int randomButton = random.nextInt(2) + 1;
+				solo.clickOnButton(solo.getString(be.mobiledatacaptator.R.string.button_open_fiche));
 
-				switch (randomButton) {
-				case 1: // open photo
-					if (countPhotos > 0) {
-						Log.e("COUNT PHOTOS", Integer.toString(countPhotos));
-						int randomPhoto = random.nextInt(countPhotos);
-						solo.clickOnView(getViewAtIndex(listViewPhotos, randomPhoto, getInstrumentation()));
-						solo.clickOnButton(solo.getString(be.mobiledatacaptator.R.string.button_display_photo));
-						solo.assertCurrentActivity("DisplayPhotoActivity", DisplayPhotoActivity.class);
-						try {
-							Thread.sleep(3000);
-						} catch (InterruptedException e) {
+				if (randomYesNo == 0) {
+					solo.clickOnButton(solo.getString(be.mobiledatacaptator.R.string.button_yes));
 
-						}
-						solo.goBackToActivity("SelectProjectActivity");
-						again = true;
-					} else // no photos for this fiche
-					{
-						solo.goBackToActivity("SelectProjectActivity");
-						again = true;
+					ArrayList<View> vEditTextesults = solo.getCurrentViews(); 
+					
+					for (int i = 1; i <= vEditTextesults.size(); i++) {
+						Log.e("VIEW", vEditTextesults.get(i).toString());
 					}
-
-					break;
-				case 2: // delete photo
-					if (countPhotos > 0) {
-						Log.e("COUNT PHOTOS", Integer.toString(countPhotos));
-						int randomPhoto = random.nextInt(countPhotos);
-						solo.clickOnView(getViewAtIndex(listViewPhotos, randomPhoto, getInstrumentation()));
-						solo.clickOnButton(solo.getString(be.mobiledatacaptator.R.string.button_delete_photo));
-
-						int randomYesNo = random.nextInt(1);
-						if (randomYesNo == 0) {
-							solo.clickOnButton(solo.getString(be.mobiledatacaptator.R.string.button_yes));
-						} else {
-							solo.clickOnButton(solo.getString(be.mobiledatacaptator.R.string.button_no));
-						}
-						
-						try {
-							Thread.sleep(3000);
-						} catch (InterruptedException e) {
-
-						}
-						solo.goBackToActivity("SelectProjectActivity");
-						again = true;
-					} else // no photos for this fiche
-					{
-						solo.goBackToActivity("SelectProjectActivity");
-						again = true;
-					}
-
-					try {
-						Thread.sleep(3000);
-					} catch (InterruptedException e) {
-
-					}
-
+					
+					
+					
+					solo.enterText(1, "test");
 					solo.goBackToActivity("SelectProjectActivity");
-					again = true;
+					
+					
+					solo.clickOnButton(solo.getString(be.mobiledatacaptator.R.string.dialog_go));
 
-					break;
 
-				case 3: // click on take photo - photo category
-					if (project.getPhotoCategories().size() > 0) {
-						int randomCategory = random.nextInt(project.getPhotoCategories().size());
-						String categoryName = project.getPhotoCategories().get(randomCategory).getName();
-
-						solo.clickOnButton(categoryName);
-					}
-					break;
-
-				case 4: // free suffix
-
-					break;
-
-				default:
-					break;
+				} else {
+					solo.clickOnButton(solo.getString(be.mobiledatacaptator.R.string.button_no));
 				}
 
+				try {
+					Thread.sleep(5000);
+				} catch (InterruptedException e1) {
+
+				}
+
+				again = true;
+
+				break;
+			case 1: // + sign
+				Log.e("+Sign", "+Sign");
+
+				break;
+			case 2: // open photo
+				Log.e("OpenFoto", "OpenFoto");
+
+				if (project.isLoadPhotoActivity() != true) {
+					Log.e("PROJECT - No Photos", project.getName());
+					again = true;
+					solo.goBack();
+				} else {
+					Log.e("PROJECT - photos", project.getName());
+
+					solo.clickOnButton(solo.getString(be.mobiledatacaptator.R.string.button_open_photo));
+					solo.assertCurrentActivity("TakePhotoActivity", TakePhotoActivity.class);
+
+					ListView listViewPhotos = (ListView) solo.getView(be.mobiledatacaptator.R.id.listViewPhotos);
+					int countPhotos = listViewPhotos.getCount();
+
+					int randomButton = random.nextInt(2) + 1;
+
+					switch (randomButton) {
+					case 1: // open photo
+						if (countPhotos > 0) {
+							Log.e("COUNT PHOTOS", Integer.toString(countPhotos));
+							int randomPhoto = random.nextInt(countPhotos);
+							solo.clickOnView(getViewAtIndex(listViewPhotos, randomPhoto, getInstrumentation()));
+							solo.clickOnButton(solo.getString(be.mobiledatacaptator.R.string.button_display_photo));
+							solo.assertCurrentActivity("DisplayPhotoActivity", DisplayPhotoActivity.class);
+							try {
+								Thread.sleep(3000);
+							} catch (InterruptedException e) {
+
+							}
+							solo.goBackToActivity("SelectProjectActivity");
+							again = true;
+						} else // no photos for this fiche
+						{
+							solo.goBackToActivity("SelectProjectActivity");
+							again = true;
+						}
+
+						break;
+					case 2: // delete photo
+						if (countPhotos > 0) {
+							Log.e("COUNT PHOTOS", Integer.toString(countPhotos));
+							int randomPhoto = random.nextInt(countPhotos);
+							solo.clickOnView(getViewAtIndex(listViewPhotos, randomPhoto, getInstrumentation()));
+							solo.clickOnButton(solo.getString(be.mobiledatacaptator.R.string.button_delete_photo));
+
+							randomYesNo = random.nextInt(1);
+
+							if (randomYesNo == 0) {
+								solo.clickOnButton(solo.getString(be.mobiledatacaptator.R.string.button_yes));
+							} else {
+								solo.clickOnButton(solo.getString(be.mobiledatacaptator.R.string.button_no));
+							}
+
+							try {
+								Thread.sleep(3000);
+							} catch (InterruptedException e) {
+
+							}
+							solo.goBackToActivity("SelectProjectActivity");
+							again = true;
+						} else // no photos for this fiche
+						{
+							solo.goBackToActivity("SelectProjectActivity");
+							again = true;
+						}
+
+						try {
+							Thread.sleep(3000);
+						} catch (InterruptedException e) {
+
+						}
+
+						solo.goBackToActivity("SelectProjectActivity");
+						again = true;
+
+						break;
+
+					case 3: // click on take photo - photo category
+						if (project.getPhotoCategories().size() > 0) {
+							int randomCategory = random.nextInt(project.getPhotoCategories().size());
+							String categoryName = project.getPhotoCategories().get(randomCategory).getName();
+
+							solo.clickOnButton(categoryName);
+						}
+						break;
+
+					case 4: // free suffix
+
+						break;
+
+					default:
+						break;
+					}
+
+				}
+
+				break;
+			case 3: // open drawing
+				Log.e("OpenDrawing", "OpenDrawing");
+
+				break;
+
+			default:
+				break;
 			}
 
 		} while (again == true);
